29cc394a2ce43e738266f086a9e6d5d3a2b266c0
girardd3@gmail.com <girardd3@gmail.com@6c60fd33-1074-9f5c-b8e1-78853defebfb>
2011-11-17 17:01:09 +0000

    setOrientation dans Element. Permet de définir l'orientation lors du rendering de l'element.

--- YouRobot/src/fr/umlv/yourobot/field/Element.java
+++ YouRobot/src/fr/umlv/yourobot/field/Element.java
@@ -4,5 +4,13 @@ import java.awt.Graphics2D;
 
 public interface Element {
 
+    /**
+     * Render the element on the graphic gd.
+     */
     public void render(Graphics2D gd);
+    
+    /**
+     * Define the orientation of the element.
+     */
+    public void setOrientation(int degree);
 }
