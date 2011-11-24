8c5a2e44d3fcac61317b7b0e807b960fda32a3f6
girardd3@gmail.com <girardd3@gmail.com@6c60fd33-1074-9f5c-b8e1-78853defebfb>
2011-11-24 22:00:46 +0000

    123

--- trunk/src/fr/umlv/yourobot/elements/bonus/Bonus.java
+++ trunk/src/fr/umlv/yourobot/elements/bonus/Bonus.java
@@ -12,10 +12,7 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Represent a bonus.
